e85a113402a7258e6e88083b07922fd0992aec0b
shiannn <b05502087@ntu.edu.tw>
2020-08-05 01:32:09 +0800

    add driver quit add sql to get data from html view

--- driver_parser.py
+++ driver_parser.py
@@ -6,13 +6,40 @@ from selenium.common.exceptions import NoSuchElementException
 import datetime, time
 import json
 
+import psycopg2
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from apscheduler.scheduler import Scheduler
+#from apscheduler.schedulers.blocking import BlockingScheduler
+
 import logging
 import sys
+import os
 logging.basicConfig(stream=sys.stdout, 
 format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
+try:
+    with open('access.json', 'r') as f:
+        access = json.load(f)
+        USERID = access['USERID']
+        PASSWORD = access['PASSWORD']
+        DRIVERLOCATION = access['DRIVERLOCATION']
+        SQLALCHEMY_DATABASE_URI = access['SQLALCHEMY_DATABASE_URI']
+except:
+    logging.warning('Please Add access json')
+    exit(0)
+
+"""
+USERID = os.environ.get('USERID')
+PASSWORD = os.environ.get('PASSWORD')
+DRIVERLOCATION = os.environ.get('CHROMEDRIVER_PATH')
+GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN")
+SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+"""
+
 SLEEP_TIME = 240
-REST_THRESHOLD = 300
+REST_THRESHOLD = 600
+TIME_DELTA = 0
 def getCourse(course_name, options_course):
     for coruse in options_course.options:
         if(coruse.text[:len(course_name)] == course_name):
@@ -48,18 +75,66 @@ def checkInPage(driver):
         except NoSuchElementException:
             time.sleep(1)
 
+def checkInLogin(driver):
+    login, sleep_times = False, 0
+    while(not login):
+        if sleep_times >= SLEEP_TIME:
+            return False
+        sleep_times += 1
+        try:
+            driver.find_element_by_id('id')
+            login = True
+            return True
+        except NoSuchElementException:
+            time.sleep(1)
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_DATABASE_URISQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Booking(db.Model):
+    """ Booking model """
+
+    __tablename__ = "Booking"
+    id = db.Column(db.Integer, primary_key=True)
+    course = db.Column(db.String(), nullable=False)
+    date = db.Column(db.String(), nullable=False) # unique
+    position = db.Column(db.String(), nullable=False)
+    last_update = db.Column(db.String(), nullable=False)
+
+    def __init__(self, course, date, position, last_update):
+        self.course = course
+        self.date = date
+        self.position = position
+        self.last_update = last_update
+    
+    def __repr__(self):
+        return "Booking {} {} {}".format(self.course, self.date, self.position)
+
+def getConfig():
+    query = Booking.query.first()
+    config = {
+        'course':query.course,
+        'date':query.date,
+        'position':query.position
+    }
+    return config
+
 def bookTKB():
     logging.warning('[TKB booking...]')
 
     options = Options()
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
+    #options.binary_location = GOOGLE_CHROME_BIN
 
     driver = webdriver.Chrome(executable_path=DRIVERLOCATION, chrome_options=options) # 選擇Chrome瀏覽器
     driver.set_window_size(400, 1800)
 
-    with open(CONFIGLOCATION, 'r') as f:
-        config = json.load(f)
+    ### get config
+    config = getConfig()
+    print(config)
 
     st = time.time()
     logging.warning('=== start {} ==='.format(st - st))
@@ -67,18 +142,23 @@ def bookTKB():
     connect_time = time.time()
     logging.warning('=== connected {} ==='.format(connect_time - st))
 
+    islogin = checkInLogin(driver)
+    if not islogin:
+        logging.warning('fail to login')
+        driver.quit()
+        return False
+
     driver.find_element_by_id('id').click()
     driver.find_element_by_id('id').send_keys(USERID)
     driver.find_element_by_id('pwd').click()
     driver.find_element_by_id('pwd').send_keys(PASSWORD)
-
-
     driver.find_element_by_link_text('送出').click()
 
     get_submit_alert, sleep_times = False, 0
     while(not get_submit_alert):
         if sleep_times >= SLEEP_TIME:
             logging.warning('no alert in login')
+            driver.quit()
             return False
         sleep_times += 1
         try:
@@ -93,20 +173,22 @@ def bookTKB():
     inCoursePage = checkInPage(driver)
     if not inCoursePage:
         logging.warning('fail to in course page')
+        driver.quit()
         return False
 
     
     logging.warning('=== into_time ===')
     today = datetime.datetime.now()
-    if today < datetime.datetime(today.year, today.month, today.day, 12, 0, 0):
+    if today < datetime.datetime(today.year, today.month, today.day, 12 - TIME_DELTA, 0, 0):
         # morning, booking noon
-        rest_time = (datetime.datetime(today.year, today.month, today.day, 12, 0, 0) - today).seconds + 1
+        rest_time = (datetime.datetime(today.year, today.month, today.day, 12 - TIME_DELTA, 0, 0) - today).total_seconds() + 1
     else:
         # afternoon, booking midnight
-        rest_time = (datetime.datetime(today.year, today.month, today.day, 23, 59, 59) - today).seconds + 2
+        rest_time = (datetime.datetime(today.year, today.month, today.day, 23 - TIME_DELTA, 59, 59) - today).total_seconds() + 2
     
-    if rest_time > REST_THRESHOLD:
+    if rest_time > REST_THRESHOLD or rest_time < 0:
         logging.warning('something wrong on rest')
+        driver.quit()
         return False
 
     driver.save_screenshot('login.png')
@@ -121,6 +203,7 @@ def bookTKB():
     clearCoursePage = checkInPage(driver)
     if not clearCoursePage:
         logging.warning('fail to clear course page')
+        driver.quit()
         return False
 
     driver.save_screenshot('clear.png')
@@ -131,6 +214,7 @@ def bookTKB():
     options_course = Select(select_course)
     course_name = getCourse(config["course"], options_course)
     if not course_name:
+        driver.quit()
         return False
     options_course.select_by_visible_text(course_name)
     logging.warning(course_name)
@@ -139,6 +223,7 @@ def bookTKB():
     options_date = Select(select_date)
     date_name = getDate(config["date"], options_date)
     if not date_name:
+        driver.quit()
         return False
     options_date.select_by_visible_text(date_name)
     logging.warning(date_name)
@@ -147,9 +232,17 @@ def bookTKB():
     options_branch = Select(select_branch)
     position_name = getPostion(config["position"], options_branch)
     if not position_name:
+        driver.quit()
         return False
     options_branch.select_by_visible_text(position_name)
     logging.warning(position_name)
+
+    has_class=driver.find_elements_by_css_selector('input[type=checkbox][value=hasClass]')
+    
+    if len(has_class) != 0:
+        logging.warning('You already have seat')
+        driver.quit()
+        return False
 
     all_checkboxs=driver.find_elements_by_css_selector('input[type=checkbox]')
     disabled_checkboxs=driver.find_elements_by_css_selector('input[type=checkbox][disabled]')
@@ -168,6 +261,7 @@ def bookTKB():
 
     if not checked:
         logging.warning('no more session')
+        driver.quit()
         return False
 
     ed = time.time()
@@ -179,6 +273,7 @@ def bookTKB():
     while(not get_submit_alert):
         if sleep_times >= SLEEP_TIME:
             print('fail to confirm submit')
+            driver.quit()
             return False
         sleep_times += 1
         try:
@@ -194,6 +289,7 @@ def bookTKB():
     while(not get_submit_alert):
         if(final_sleep_times >= SLEEP_TIME):
             print('No final alert')
+            driver.quit()
             return False
         final_sleep_times += 1
         try:
@@ -208,32 +304,31 @@ def bookTKB():
     ### Todo: try 5 times until succeed with function call
     ### Todo: scheduling and get config each trial
     ### Todo: put the root directory of program into config
+    driver.quit()
     return True
 
-MORNING_LOGIN = "11:55"
-MIDNIGHT_LOGIN = "23:55"
+def printHello():
+    print('hello')
+    return
+
 def main():
-    import schedule
-    schedule.every().day.at(MORNING_LOGIN).do(bookTKB)
-    schedule.every().day.at(MIDNIGHT_LOGIN).do(bookTKB)
+    sched = Scheduler()
+    #sched = BlockingScheduler()
+    sched.start()
+    sched.add_cron_job(bookTKB, hour=11, minute=50)
+    sched.add_cron_job(bookTKB, hour=23, minute=50)
 
-    logging.warning('running up...')
-    while(True):
-        schedule.run_pending()
+    sched.add_cron_job(bookTKB, hour=1, minute=31)
+    #sched.add_cron_job(bookTKB, hour=16, minute=50)
+    #sched.add_job(bookTKB, 'cron', hour=3, minute=55)
+    #sched.add_job(bookTKB, 'cron', hour=15, minute=55)
+    #sched.add_job(bookTKB, 'cron', hour=8, minute=12)
+    logging.warning('running...')
 
-    #bookTKB()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
     
 if __name__ == '__main__':
     LOGIN_URL = 'https://bookseat.tkblearning.com.tw/book-seat/student/login/toLogin'
-    try:
-        with open('access.json', 'r') as f:
-            access = json.load(f)
-            USERID = access['USERID']
-            PASSWORD = access['PASSWORD']
-            DRIVERLOCATION = access['DRIVERLOCATION']
-            CONFIGLOCATION = access['CONFIGLOCATION']
-    except:
-        logging.warning('Please Add access json')
-        exit(0)
-
     main()
